d0629fbf8f747e90edd7d7ce7a272f510ce875ad
Documenter.jl <documenter@juliadocs.github.io>
2025-05-22 19:28:45 +0000

    build based on 3bc93d0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SemiAnalyticInstabilityAnalysis","category":"page"},{"location":"#SemiAnalyticInstabilityAnalysis","page":"Home","title":"SemiAnalyticInstabilityAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SemiAnalyticInstabilityAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SemiAnalyticInstabilityAnalysis]","category":"page"}]
+[{"location":"#SemiAnalyticInstabilityAnalysis","page":"Home","title":"SemiAnalyticInstabilityAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SemiAnalyticInstabilityAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
